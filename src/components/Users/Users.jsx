f3d6177b0640dc0020b2c5f088b8570e045e33a1
Грачев Александр <agrachev@iflex.ru>
2021-03-08 20:09:12 +0300

    Users page added. Pagination added too.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,87 @@
-import React, { useEffect } from "react";
+import React, { Component, useEffect } from "react";
 import c from './Users.module.css';
 import * as axios from "axios";
 import userPhoto from '../../assets/images/user.png';
 
-const Users = ({users, follow, unfollow, setUsers}) => {
+// const Users = ({users, follow, unfollow, setUsers}) => {
+//
+//     useEffect(() => {
+//         if (!users.length) {
+//             axios
+//                 .get('https://social-network.samuraijs.com/api/1.0/users')
+//                 .then((response) => {
+//                     setUsers(response.data.items)
+//                 })
+//         }
+//     })
+//
+//     return (
+//         <div>
+//             {users.map(u => {
+//                 return (
+//                     <div>
+//                         <span>
+//                             <div>
+//                                 <img src={u.photos.small ? u.photos.small : userPhoto} alt="" className={c.userPhoto} />
+//                             </div>
+//                             <div>
+//                                 {u.followed
+//                                     ? <button onClick={()=>unfollow(u.id)}>Unfollow</button>
+//                                     : <button onClick={()=>follow(u.id)}>Follow</button>}
+//                             </div>
+//                         </span>
+//                         <span>
+//                             <span>
+//                                 <div>{u.name}</div>
+//                                 <div>{u.status}</div>
+//                             </span>
+//                             <span>
+//                                 <div>{"u.location.country"}</div>
+//                                 <div>{"u.location.city"}</div>
+//                             </span>
+//                         </span>
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//     );
+// };
 
-    useEffect(() => {
-        if (!users.length) {
-            axios
-                .get('https://social-network.samuraijs.com/api/1.0/users')
-                .then((response) => {
-                    setUsers(response.data.items)
-                })
+class Users extends Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?pages=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+
+    onPageChanged = pageNumber => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+            })
+    }
+
+    render() {
+        const { users, follow, unfollow, totalUsersCount, pageSize, currentPage, setCurrentPage } = this.props;
+        const pagesCount = Math.ceil(totalUsersCount / pageSize);   // При totalUsersCount 19 и pageSize 5 не теряются последние пользователи
+        let pages = [];
+        console.log(totalUsersCount)
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
         }
-    })
 
-    return (
-        <div>
-            {users.map(u => {
-                return (
-                    <div>
+        return (
+            <div>
+                <div>
+                    {pages.map(p => <span className={currentPage === p && c.selectedPage} onClick={() => this.onPageChanged(p)}>{p}</span>)}
+                </div>
+                {users.map(u => {
+                    return (
+                        <div>
                         <span>
                             <div>
                                 <img src={u.photos.small ? u.photos.small : userPhoto} alt="" className={c.userPhoto} />
@@ -30,7 +92,7 @@ const Users = ({users, follow, unfollow, setUsers}) => {
                                     : <button onClick={()=>follow(u.id)}>Follow</button>}
                             </div>
                         </span>
-                        <span>
+                            <span>
                             <span>
                                 <div>{u.name}</div>
                                 <div>{u.status}</div>
@@ -40,11 +102,12 @@ const Users = ({users, follow, unfollow, setUsers}) => {
                                 <div>{"u.location.city"}</div>
                             </span>
                         </span>
-                    </div>
-                )
-            })}
-        </div>
-    );
-};
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
+}
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,13 +1,18 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT';
 
 const initialState = {
     users: [
         // {id: 1, photoUrl: 'https://i.pinimg.com/originals/29/ee/44/29ee44185cf69f20f11291716a93dc37.jpg', followed: false, fullName: "Alex", status: "Hi there", location: {city: "Moscow", country: "Russia"}},
         // {id: 2, photoUrl: 'https://i.pinimg.com/originals/9a/da/3b/9ada3bc305a1f45eab527f60da172d53.png', followed: true, fullName: "Sveta", status: "Life is good!", location: {city: "Kiev", country: "Ukraine"}},
         // {id: 3, photoUrl: 'https://99px.ru/sstorage/41/2010/05/image_410705101709499256168.jpg', followed: false, fullName: "Vika", status: "learn redux, guys xD", location: {city: "Rostov", country: "Russia"}}
-    ]
+    ],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 2
 }
 
 const usersReducer = ( state = initialState, action ) => {
@@ -35,7 +40,17 @@ const usersReducer = ( state = initialState, action ) => {
         case SET_USERS:
             return {
                 ...state,
-                users: [...state.users, ...action.users]    // ...спред action.users, т.к. action.users-массив, склеиваем 2 массива в 1
+                users: action.users    // ...спред action.users, т.к. action.users-массив, склеиваем 2 массива в 1
+            };
+        case SET_CURRENT_PAGE:
+            return {
+                ...state,
+                currentPage: action.currentPage
+            };
+        case SET_TOTAL_USERS_COUNT:
+            return {
+                ...state,
+                totalUsersCount: action.count
             };
         default:
             return state;
@@ -58,6 +73,18 @@ export const unFollowAC = userId => {
 export const setUsersAC = users => {
     return {
         type: SET_USERS, users
+    }
+}
+
+export const setCurrentPageAC = currentPage => {
+    return {
+        type: SET_CURRENT_PAGE, currentPage
+    }
+}
+
+export const setUsersTotalCountAC = totalUsersCount => {
+    return {
+        type: SET_TOTAL_USERS_COUNT, count: totalUsersCount
     }
 }
 
