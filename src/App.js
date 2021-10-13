4b03db9519c2bc25f005efa34a1f1d5a337c5179
Alexander <agrachev@iflex.ru>
2021-10-13 12:23:07 +0300

    react suspense. react lazy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import Navbar from "./components/Navbar/Navbar";
 import {Route, withRouter} from "react-router-dom";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import {initializeApp} from "./redux/app-reducer";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import Preloader from "./components/common/Preloader/Preloader";
+
 import "./App.css";
+
+// в bundle не попадёт, а загрузится по мере необходимости
+const ProfileContainer = lazy( () => import('./components/Profile/ProfileContainer'));
+const DialogsContainer = lazy( () => import('./components/Dialogs/DialogsContainer'));
+
+
 
 class App extends React.Component {
     componentDidMount() {
@@ -26,13 +31,15 @@ class App extends React.Component {
 
         return (
             <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
+                <HeaderContainer />
+                <Navbar />
                 <div className='app-wrapper-content'>
-                    <Route exact path='/dialogs' render={() => <DialogsContainer/>}/>
-                    <Route path='/profile/:userId?' render={() =>
-                        <ProfileContainer/>}/> {/*теперь /profile без парамаетра не открывается, указываем что он не обязателен*/}
-                    <Route path='/users' render={() => <UsersContainer/>}/>
+                    <Suspense fallback={<Preloader />}>
+                        {/*теперь /profile без парамаетра не открывается, указываем что он не обязателен*/}
+                        <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
+                        <Route exact path='/dialogs' render={() => <DialogsContainer />}/>
+                    </Suspense>
+                    <Route path='/users' render={() => <UsersContainer />}/>
                     <Route path='/news' component={News}/>
                     <Route path='/music' component={Music}/>
                     <Route path='/login' component={LoginPage}/>
