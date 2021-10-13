0d7ef99f44f1371c8962b3bf382d55f2d6bdf8f0
Alexander <agrachev@iflex.ru>
2021-10-13 23:43:59 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import store from './redux/redux-store';
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
     // Разные люди хотят видеть здесь иные значения, поэтому нельзя 'https://alexandr1111.github.io/social-network',
     // не будет работать на localhost
     // hashRouter - работает на якорях как раньше, решает проблему с 404 при обновлении страницы после
-    // https://alexandr1111.github.io/social-network/profile(или другие адреса)
+    // https://alexandr1111.github.io/social-network/profile(или другие адреса) если не писать basename
     <Router basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App />
